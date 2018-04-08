e76b669ac7a37991fe9cb78b51a5fb00b39ecf47
John Coffelt <33336503+JohnRCoffelt@users.noreply.github.com>
2018-04-08 16:49:29 -0700

    Updated with Phase 3

--- Assig6/Assig6.java
+++ Assig6/Assig6.java
@@ -1,29 +1,15 @@
 //Assignment 6 CST 338
 //John Coffelt, Michael Rose, Christopher Carson, Raul Ramirez
 
-//Phase 3 - Michael Rose
-public class Assig6 {
-   public static void main(String[] args) {
-
-      // CREATE THE GAME
-      int NUM_CARDS_PER_HAND = 7;
-      int NUM_PLAYERS = 2;
-      int numPacksPerDeck = 1;
-      int numJokersPerPack = 0;
-      int numUnusedCardsPerPack = 0;
-      GameModel.Card[] unusedCardsPerPack = null;
-
-      GameModel gameModel = new GameModel(numPacksPerDeck, numJokersPerPack, numUnusedCardsPerPack, unusedCardsPerPack,
-            NUM_PLAYERS, NUM_CARDS_PER_HAND);
-
-      // initialize View
-      GameView gameView = new GameView("CardGame");
-
-      GameController gameController = new GameController(gameModel, gameView);
-
-      gameController.StartGame();
-
-      System.exit(0);
-   }// end Main
-
+//Phase 3
+public class Assig6
+{   
+   public static void main(String[] args) throws InterruptedException
+   {  
+      Model cardGameModel = new Model();
+      View cardGameView = new View();
+      Controller cardGameController = new Controller(cardGameModel, cardGameView);
+      cardGameView.addController(cardGameController);
+      cardGameController.init();
+   }//End Main
 }// end Assig6
